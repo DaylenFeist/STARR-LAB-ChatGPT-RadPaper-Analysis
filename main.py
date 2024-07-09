cf37a84499637d819324fa68c2090273ec534924
DaylenFeist <daylen.feist1@gmail.com>
2024-07-09 15:00:42 -0600

    Added functionality to export results to .xlsx

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ Description:
 """
 from gpt_analysis import gpt_parse
 import ast
+import xlsxwriter
 
 def main():
   # Gather the names of all pdfs provided (at the moment, in the same directory)
@@ -42,11 +43,25 @@ def main():
     final_results = prelim_results + secondary_results
     answer_matrix.append(final_results)
   print(answer_matrix)
+  write_to_excel(answer_matrix, pdf_names)
 
 #TODO: function to find all pdfs in directory
 def find_file_names():
+    #finds all files in the same directory, with the extension .pdf, and returns them in a list
   file_names = ["3_MeV_Proton_Irradiation_of_Commercial_State_of_the_Art_Photonic_Mixer_Devices.pdf", "RADON-5E_portable_pulsed_laser_simulator_description_qualification_technique_and_results_dosimetry_procedure.pdf"]
   return file_names
+
+def write_to_excel(matrix, papers):
+    #writes the final answers and the names of papers to an excel book,
+    workbook = xlsxwriter.Workbook('results.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    for row in range(len(papers)):
+        worksheet.write(row, 0, papers[row])
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            worksheet.write(row, col+1, matrix[row][col])
+    workbook.close()
 
 #openai.api_key = 'sk-proj' #currently an environment variable, but can be put here
 
