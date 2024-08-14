9b9676c87cd0472860514e69568a1de214f615bd
Shiv Krishnaswamy <s2krishn@uwaterloo.ca>
2024-08-14 15:36:40 -0600

    Added more comments for readability

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 """
 Title: OpenAI--gpt-4o--Radiation Paper Analysis
 Supervisor: Dr. Li Chen (ECE--USASK)
-Creator: Daylen Feist, Shiv Krishnaswamy , Juan Arguello Escalante
+Creator: Daylen Feist, Shiv Krishnaswamy, Juan Arguello Escalante
 Date: 2024-07-05
 
 Description:
-    This project has the goal of analyzing papers published by the IEEE
-    Radiation Effects Data Workshop (REDW).
+    This project has the goal of analyzing papers published by the IEEE Radiation Effects Data Workshop (REDW)
     With the analyzed results, a concise and accurate database for all components tested can be made
     which would have tremendous value to industry, and community research
 """
+
 from gpt_analysis import gpt_parse
-import xlsxwriter
-import os
 import threading as thread
+import xlsxwriter
 import sort
+import os
+
+
 def main():
-    # Gather the names of all pdfs provided (at the moment, in the same directory)
+    # A list containing the paths of all the papers (from find_papers function)
     paper_list = find_papers()
+    # Get the number of papers in the list
     num_papers = len(paper_list)
+    # Create an empty list "num_papers" long. Used for multithreading the parsing process
     thread_list = [None] * num_papers
+    # List for answers returned from parsing each paper
     answer_matrix = []
+    # Loop num_papers times. From 0 -> num_papers-1
     for x in range(num_papers):
+        # Hold the path of the current paper
         paper = paper_list[x]
+        # Assign the null variable at the index of x to a thread processing the current paper
+        # Thread runs the process_paper function and passes the current paper and the answer list as a parameter
         thread_list[x] = thread.Thread(target=process_paper, args=(paper, answer_matrix,))
+        # Start the thread
         thread_list[x].start()
+    # Loop num_papers times again
     for y in range(num_papers):
+        # Join threads -> wait until all threads completed before executing the next code
         thread_list[y].join()
+    # Create an excel spreadsheet organizing prompt answers of all papers
     write_to_excel(answer_matrix, paper_list)
+
 
 def process_paper(paper, answer_matrix):
     # Gather info about paper, such as (author, part no. type, manufacturer, **important** type of testing)
@@ -46,26 +60,32 @@ def process_paper(paper, answer_matrix):
     targeted_prompt = """Please answer the following questions, as concisely as possible, and with a heavy emphasis on numbers instead of words.
                 Use standard text and do not provide citations for each of your answers. 
                 Answer each question, and separate the answers with a "ø" character as a delimiter.
-                If you are unable to answer the question accurately, provide the answer N/A.\n""" + ". ".join(
-        targeted_questions)
+                If you are unable to answer the question accurately, provide the answer N/A.\n""" + ". ".join(targeted_questions)
     secondary_results = gpt_parse(assistant_prompt, targeted_prompt, paper)
     secondary_results = secondary_results.split("ø")
     final_results = prelim_results + secondary_results
     print(final_results)
     answer_matrix.append(final_results)
-# This function works to get all the papers from the ExamplePaper directory
+
+
 def find_papers():
+    # Get the path of the sub-directory where the SMD type of papers are held.
+    # Only processing SMD papers right now
     directory = 'Papers_Sorted/SMD'
+    # Create a list for the names of the papers
     paperList = []
+    # For every paper within directory
     for filename in os.listdir(directory):
+        # Append the path of the current paper to the list
         paperList.append(os.path.join(directory, filename))
+    # Return the list
     return paperList
 
+
 def write_to_excel(matrix, papers):
-    #writes the final answers and the names of papers to an excel book,
+    # Writes the final answers and the names of papers to an excel book
     workbook = xlsxwriter.Workbook('results.xlsx')
     worksheet = workbook.add_worksheet()
-
     for row in range(len(papers)):
         worksheet.write(row, 0, papers[row])
     for row in range(len(matrix)):
@@ -73,14 +93,15 @@ def write_to_excel(matrix, papers):
             worksheet.write(row, col+1, matrix[row][col])
     workbook.close()
 
-#openai.api_key = 'sk-proj' #currently an environment variable, but can be put here
+# openai.api_key = 'sk-proj' #currently an environment variable, but can be put here
 
-name="Radiation Effects Researcher"
+# Setup for the chat_gpt engine and definition of the prompt/questions
+name = "Radiation Effects Researcher"
 assistant_instructions = "You are a radiation effects reasearcher. Use your knowledge to give very concise and numerical answers to the questions. Please do not give citations."
 model = "gpt-4o"
 assistant_prompt = [name, assistant_instructions, model]
 
-#author, part no. type, manufacturer, **important** type of testing)
+# author, part no. type, manufacturer, **important** type of testing)
 questions = ["What is the first authors name, in the format (J. Doe)", "What is the Part No. or name if that is not available",
              "What is the type of part (eg, switching regulator)", "Who is the manufacturer",
              "What type of testing was done: Respond to this question with \"TID\" for Total Ionizing Dose testing, \"SEE\" for heavy ion, proton, laser, or neutron testing, or \"OTHER\" if you are not completely 100% sure"
--- sort.py
+++ sort.py
@@ -1,35 +1,54 @@
 """
 Description:
-    This file serves the purpose of sorting IEEE workshop papers into distinct categories
-    to use targeted analysis.
-    """
-
-import time
+This file serves the purpose of sorting IEEE workshop papers into distinct categories
+to use targeted analysis.
+"""
 
 from gpt_analysis import gpt_parse
-import os
-import shutil
 import threading as thread
+import shutil
+import time
+import os
+
 
 def main():
-    # Gather the names of all pdfs provided (at the moment, in the same directory)
+    # A list containing the paths of all the papers (from find_papers function)
     paper_list = find_papers()
+    # Get the number of papers in the list
     num_papers = len(paper_list)
+    # Create an empty list "num_papers" long. Used for multithreading the sorting process
     thread_list = [None] * num_papers
+    # Create the directory "Papers_Sorted" if it doesn't already exist. Used as the place where sorted papers will go
     if not os.path.exists("Papers_Sorted"):
         os.makedirs("Papers_Sorted")
+    # Loop num_papers times. From 0 -> num_papers-1
     for x in range(num_papers):
+        # Hold the path of the current paper
         cur_paper = paper_list[x]
+        # Assign the null variable at the index of x to a thread processing the current paper
+        # Thread runs the sort_paper function and passes the current paper as a parameter
         thread_list[x] = thread.Thread(target=sort_papers, args=(cur_paper,))
+        # Start the thread
         thread_list[x].start()
+    # Loop num_papers times again
     for y in range(num_papers):
+        # Join threads -> wait until all threads completed before executing the next code
         thread_list[y].join()
 
+
 def sort_papers(paper):
+    # Variable which holds the type of paper passed into the function
+    # The paper is parsed using the GPT engine and answers the prompt below
     paper_type = gpt_parse(assistant_prompt, prompt, paper)
+    # Print paper type to console
     print(paper_type)
+    #
     time.sleep(2)
+    # Variable holding the name of the directory where sorted papers go
     baseFolder = 'Papers_Sorted'
+    # If the paper is of type "LAB", then a sub-directory within Papers_Sorted will be created named LAB
+    #       (for papers of these types) if this sub-directory isn't already there
+    # The paper is then copied to that sub-directory and the same process applies to the other 4 paper types
     if paper_type == "LAB":
         if not os.path.exists(baseFolder + "/LAB/"):
             os.makedirs(baseFolder + "/LAB/")
@@ -53,22 +72,29 @@ def sort_papers(paper):
     else:
         print("user")
 
+
 def find_papers():
+    # Get the path of directory of where the unsorted papers are held
     directory = 'ExamplePapers'
+    # Create a list for the names of the papers
     paperList = []
+    # For every paper within directory
     for filename in os.listdir(directory):
+        # Append the path of the current paper to the list
         paperList.append(os.path.join(directory, filename))
+    # Return the list
     return paperList
 
 
-#openai.api_key = 'sk-proj' #currently an environment variable, but can be put here
+# openai.api_key = 'sk-proj' (currently an environment variable, but can be put here)
 
-name="Radiation Effects Researcher"
+# Setup for the chat_gpt engine and definition of the prompt/questions
+name = "Radiation Effects Researcher"
 assistant_instructions = "You are a radiation effects reasearcher. Use your knowledge to categorize this paper into one of the five categories. Only respond with one of the five three letter categories. Please do not give citations."
 model = "gpt-4o-mini"
 assistant_prompt = [name, assistant_instructions, model]
 
-#author, part no. type, manufacturer, **important** type of testing)
+# author, part no. type, manufacturer, **important** type of testing)
 questions = """There are five types of papers: 
             The first are \"Laboratory Capabilities/Facility Equipment/Simulator\", which detail the capacities of a location, or university for use in research.
             The second are \"Testing Methods\", which detail specific methods of testing, without any devices being tested in the paper.
@@ -78,7 +104,7 @@ questions = """There are five types of papers:
             In the same order, respond with \"LAB\", \"TST\", \"PHE\", \"CMP\", or \"SMD\", for the category that the paper best fits."""
 
 # TODO: possibly make prompt better
-prompt="""Please answer the following question, as concisely as possible, with a single word answer as outlined in the question.
+prompt = """Please answer the following question, as concisely as possible, with a single word answer as outlined in the question.
             Classify this paper into one of the following categories: """ + questions + """
             Use standard text and do not provide citations for each of your answer.
             Answer the question with the keyword for one of the 5 papers.
